6f4545d9cfb20f1ee2ba329a89e9d6695c589ddb
Ian Morland <ian@morland.me>
2021-06-28 20:29:36 +0100

    Flarum 1.0 (#9)
    * Flarum 1.0
    
    * Remove copyright notice
    
    * Add tsconfig
    
    * Switch from `.then()` to async/await
    
    * Perform double API request to fix missing user attributes
    
    Co-authored-by: David Wheatley <hi@davwheat.dev>

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,9 +1,25 @@
-import app from 'flarum/app';
-import alertEmailConfirmation from 'flarum/utils/alertEmailConfirmation';
-import { extend, override } from 'flarum/extend';
-import LogInModal from 'flarum/components/LogInModal';
-import DiscussionList from 'flarum/components/DiscussionList';
-import SignUpModal from 'flarum/components/SignUpModal';
+import app from 'flarum/forum/app';
+import alertEmailConfirmation from 'flarum/forum/utils/alertEmailConfirmation';
+import { extend, override } from 'flarum/common/extend';
+import LogInModal from 'flarum/forum/components/LogInModal';
+import DiscussionList from 'flarum/forum/components/DiscussionList';
+import SignUpModal from 'flarum/forum/components/SignUpModal';
+
+/**
+ * Hits the API endpoint by calling `app.store.find('')`, which will load
+ * all initial data that the user should have set.
+ */
+async function loadBaseApiData() {
+    await app.store.find('');
+}
+
+/**
+ * Hides the provided modal, and calls the `loaded` method on that modal.
+ */
+function closeModal(modalInstance) {
+    modalInstance.hide();
+    modalInstance.loaded?.call(modalInstance);
+}
 
 app.initializers.add('fof-realtimelogin', () => {
     extend(DiscussionList.prototype, 'view', function () {
@@ -12,32 +28,35 @@ app.initializers.add('fof-realtimelogin', () => {
         }
     });
 
-    override(SignUpModal.prototype, 'onsubmit', function (original, e) {
+    override(SignUpModal.prototype, 'onsubmit', async function (original, e) {
         e.preventDefault();
 
         this.loading = true;
 
         const data = this.submitData();
 
-        app.request({
+        await app.request({
             url: app.forum.attribute('baseUrl') + '/register',
             method: 'POST',
             data,
             errorHandler: this.onerror.bind(this),
-        }).then(() => {
-            app.store.find('users', data.username).then((user) => {
-                app.session.user = user;
-                app.store.find('' + '' + '').then(() => {
-                    alertEmailConfirmation(app);
-                    app.newLogin = true;
-                    this.hide();
-                    this.loaded.bind(this);
-                });
-            });
         });
+
+        const user = await app.store.find('users', {
+            filter: { username: data.username },
+            page: { limit: 1 },
+        });
+
+        app.session.user = user[0];
+        await loadBaseApiData();
+
+        alertEmailConfirmation(app);
+        app.newLogin = true;
+
+        closeModal(this);
     });
 
-    override(LogInModal.prototype, 'onsubmit', function (original, e) {
+    override(LogInModal.prototype, 'onsubmit', async function (original, e) {
         e.preventDefault();
 
         this.loading = true;
@@ -46,15 +65,18 @@ app.initializers.add('fof-realtimelogin', () => {
         const password = this.password();
         const remember = this.remember();
 
-        app.session.login({ identification, password, remember }, { errorHandler: this.onerror.bind(this) }).then(() => {
-            app.store.find('users', identification).then((user) => {
-                app.session.user = user;
-                app.store.find('').then(() => {
-                    app.newLogin = true;
-                    this.hide();
-                    this.loaded.bind(this);
-                });
-            });
+        await app.session.login({ identification, password, remember }, { errorHandler: this.onerror.bind(this) });
+
+        const tempUser = await app.store.find('users', {
+            filter: { username: identification },
+            page: { limit: 1 },
         });
+
+        app.session.user = await app.store.find('users', tempUser[0].id());
+        await loadBaseApiData();
+
+        app.newLogin = true;
+
+        closeModal(this);
     });
 });

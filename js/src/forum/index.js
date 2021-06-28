70886a2d3b52724dcb2c42c3cb05574028294957
David Wheatley <hi@davwheat.dev>
2021-06-28 19:33:56 +0100

    Switch from `.then()` to async/await

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -5,6 +5,22 @@ import LogInModal from 'flarum/forum/components/LogInModal';
 import DiscussionList from 'flarum/forum/components/DiscussionList';
 import SignUpModal from 'flarum/forum/components/SignUpModal';
 
+/**
+ * Hits the API endpoint by calling `app.store.find('')`, which will load
+ * all initial data that the user should have set.
+ */
+async function loadBaseApiData() {
+    await app.store.find('');
+}
+
+/**
+ * Hides the provided modal, and calls the `loaded` method on that modal.
+ */
+function closeModal(modalInstance) {
+    modalInstance.hide();
+    modalInstance.loaded?.call(modalInstance);
+}
+
 app.initializers.add('fof-realtimelogin', () => {
     extend(DiscussionList.prototype, 'view', function () {
         if (app.newLogin) {
@@ -12,32 +28,35 @@ app.initializers.add('fof-realtimelogin', () => {
         }
     });
 
-    override(SignUpModal.prototype, 'onsubmit', function (original, e) {
+    override(SignUpModal.prototype, 'onsubmit', async function (original, e) {
         e.preventDefault();
 
         this.loading = true;
 
         const data = this.submitData();
 
-        app.request({
+        await app.request({
             url: app.forum.attribute('baseUrl') + '/register',
             method: 'POST',
             data,
             errorHandler: this.onerror.bind(this),
-        }).then(() => {
-            app.store.find('users', { filter: { username: data.username }, page: { limit: 1 } }).then((user) => {
-                app.session.user = user[0];
-                app.store.find('' + '' + '').then(() => {
-                    alertEmailConfirmation(app);
-                    app.newLogin = true;
-                    this.hide();
-                    this.loaded.bind(this);
-                });
-            });
         });
+
+        const user = await app.store.find('users', {
+            filter: { username: data.username },
+            page: { limit: 1 },
+        });
+
+        app.session.user = user[0];
+        await loadBaseApiData();
+
+        alertEmailConfirmation(app);
+        app.newLogin = true;
+
+        closeModal(this);
     });
 
-    override(LogInModal.prototype, 'onsubmit', function (original, e) {
+    override(LogInModal.prototype, 'onsubmit', async function (original, e) {
         e.preventDefault();
 
         this.loading = true;
@@ -46,15 +65,18 @@ app.initializers.add('fof-realtimelogin', () => {
         const password = this.password();
         const remember = this.remember();
 
-        app.session.login({ identification, password, remember }, { errorHandler: this.onerror.bind(this) }).then(() => {
-            app.store.find('users', { filter: { username: identification }, page: { limit: 1 } }).then((user) => {
-                app.session.user = user[0];
-                app.store.find('').then(() => {
-                    app.newLogin = true;
-                    this.hide();
-                    this.loaded.bind(this);
-                });
-            });
+        await app.session.login({ identification, password, remember }, { errorHandler: this.onerror.bind(this) });
+
+        const user = await app.store.find('users', {
+            filter: { username: identification },
+            page: { limit: 1 },
         });
+
+        app.session.user = user[0];
+        await loadBaseApiData();
+
+        app.newLogin = true;
+
+        closeModal(this);
     });
 });
